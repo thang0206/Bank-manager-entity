41a93dde3796a987b2011157f8deff4d6ce35061
ThangLee <112875565+thang0206@users.noreply.github.com>
2023-05-08 13:27:02 +0700

    Initial commit

---
+++ BankManage/FCredit.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.Odbc;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace BankManage
+{
+    public partial class FCredit : Form
+    {
+        Customer customer;
+        DBConnection DbConnection = new DBConnection();
+        CreditDAO creditDAO = new CreditDAO();
+        Credit credit;
+        
+        public FCredit(Customer choosedCustomer)
+        {
+            InitializeComponent();
+            customer = choosedCustomer;
+        }
+
+        private void FCredit_Load(object sender, EventArgs e)
+        {
+            credit = new Credit(customer.Stk);
+
+            credit = creditDAO.Get(credit);
+
+            txtSothe.Text = credit.ID;
+            txtHanmuc.Text = credit.HanMuc.ToString();
+            txtMoneyUsed.Text = credit.UsedMoney.ToString();
+
+            if (DateTime.Now >= credit.NgayDaoHan)
+            {
+                CustomerDAO customerDAO = new CustomerDAO();
+                customer.Money = customer.Money - credit.UsedMoney;
+
+                if (customer.Money < 50000 )
+                {
+                    MessageBox.Show("Số tiền trong tài khoản gốc không đủ để trả thế chấp. Bạn sẽ bị khóa thẻ");
+                    btnDelete.Enabled = false;
+                    btnSubmit.Enabled = false;
+                }
+                else
+                {
+                    customerDAO.UpdateMoney(customer);
+                }
+            }
+
+            if (credit.UsedMoney > credit.HanMuc)
+            {
+                MessageBox.Show("Bạn đã xài quá hạn mức. Vui lòng gửi tiền vào thẻ để tiếp tục sử dụng");
+            }
+            else
+            {
+                if (txtSothe.Text == "")
+                {
+                    btnSubmit.Enabled = true;
+                    btnDelete.Enabled = false;
+                }
+                else
+                {
+                    btnDelete.Enabled = true;
+                    btnSubmit.Enabled = false;
+                }
+            }            
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            string MaThe = random.NextString(10);
+
+            if (txtHanmuc.Text == "")
+            {
+                MessageBox.Show("Vui long nhap han muc the");
+            }
+            else
+            {
+                Credit credit = new Credit(customer.Stk, MaThe, Convert.ToInt32(txtHanmuc.Text), 0, cmbMethod.Text, DateTime.Now, DateTime.Now.AddMonths(1));
+                creditDAO.Create(credit);
+
+                MessageBox.Show("Chúc mừng bạn đã mở thẻ tín dụng thành công:\nSố thẻ: " + MaThe + "\nHạn mức cho phép:" + txtHanmuc.Text + "\nVui lòng thanh toán dư nợ vào ngày " + DateTime.Now.Day + " của tháng tiếp theo, nếu phát sinh giao dịch");
+            }
+            FCredit_Load(sender, e);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            Credit credit = new Credit(customer.Stk, txtSothe.Text);
+            creditDAO.Delete(credit);
+            FCredit_Load(sender, e);
+            btnDelete.Enabled = false;
+        }
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            ClearInformation();
+            btnDelete.Enabled = false;
+        }
+        private void ClearInformation()
+        {
+            txtHanmuc.Clear();
+            txtMoneyUsed.Clear();
+            txtSothe.Clear();
+            cmbMethod.Text = "";
+        }
+
+        private void cmbMethod_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbMethod.Text != "")
+            {
+                btnSubmit.Enabled = true;
+            }
+        }
+    }
+}
